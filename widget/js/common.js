4ec1c7de0438f834da1602ab48c2a700d5ef96a0
HAO <whwsdgl@gmail.com>
2017-10-30 19:34:52 +0800

    first commit

---
+++ widget/js/common.js
@@ -0,0 +1,83 @@
+function erplogin(user, pass, db) {
+	var data = {
+		login: user,
+		pass: pass,
+		dbId: db,
+		rememberMe: true
+	}
+	api.ajax({
+    url: url.erplogin,
+    method: 'post',
+		dataType: 'text',
+    data: {
+        values: data
+    }
+}, function(ret, err) {
+    if (ret) {
+        api.alert({ msg: (ret+'OK') });
+    } else {
+        api.alert({ msg: ('ERR'+err) });
+    }
+});
+}
+
+function login(workid,password,cb,errcb){
+	var data = {
+		workid:workid,
+		password:password,
+		deviceid:api.deviceId
+	}
+	post(url.login,data,function(ret,err){
+		if(err){
+			if(errcb){
+				errcb(err.msg);
+			}
+		}else if(ret.retStatus == 'OK'){
+			if(cb){
+				cb(ret.retValue);
+			}
+		}else{
+			if(errcb){
+				errcb(JSON.stringify(ret));
+			}
+		}
+	});
+}
+function executeSql(db, sql, cb, errCb){
+	db.executeSql({
+		name : config.DB_NAME,
+		sql : sql,
+	},function(ret, err){
+		if(err){
+			if(errCb)
+				errCb(err.msg)
+		}else{
+			if(cb)
+				cb()
+		}
+	})
+}
+function selectSql(db, sql, cb, errCb){
+	db.selectSql({
+		name : config.DB_NAME,
+		sql : sql
+	}, function(ret, err){
+		if(err){
+			if(errCb)
+				errCb(err.msg)
+		}else{
+			if(cb)
+				cb(ret.data)
+		}
+	})
+}
+function toast(msg,time){
+	if(!time){
+		time = 2000
+	}
+	api.toast({
+	    msg:msg,
+	    duration:time,
+	    location:'bottom'
+    });
+}
---
+++ widget/js/config.js
@@ -0,0 +1,56 @@
+var config={
+	//ServerIp:'http://192.168.6.104:8089'
+	//ServerIp:'http://192.168.125.118:8089'
+	ServerIp:'http://192.168.6.109:8089'
+}
+
+var url={
+	download:config.ServerIp+'/barCode/getBarList',//download barid list
+	login:config.ServerIp+'/employees/appLogin',//login
+	uploadgroup:config.ServerIp+'/plantWorkGroup/createWorkGroup',
+	downloadgroup:config.ServerIp+'/plantWorkGroup/getWorkGroup',
+	deletegroup:config.ServerIp+'/plantWorkGroup/dismissWorkGroup',
+	uploadbarcode:config.ServerIp+'/barCode/putBarCodeScanInfo',
+	history:config.ServerIp+'/barCode/getWorkLoadOfDay',
+	barinfo:config.ServerIp+'/barCode/getBarcodeInfo',
+	clearbar:config.ServerIp+'/barCode/deleteBarScanInfoOnSvr',
+	erplogin: config.ServerIp+'/users/login',
+	getCurrentOrders: config.ServerIp+'/aluveneerOrders/getCurrentOrders'
+};
+var config={
+	USER_KEY:'userInfo',
+	GROUP_KEY:'group',
+	DB_NAME:'baoye',
+	DB_PATH:'fs://test.db',
+	TABLE_BARCODE : 'CREATE TABLE IF NOT EXISTS Barcodetest (_id varchar(32), barId varchar(16), barScanDate varchar(32), projName varchar(256), status int default(0),groupId varchar(32),remark varchar(20),PRIMARY KEY(_id))',
+//	TABLE_GROUP : 'CREATE TABLE IF NOT EXISTS Workgroup (workname varchar(255),tel varchar(255),workid varchar(255),PRIMARY KEY(workid))'
+}
+var pages={
+	index:{name:'root', url:'../index.html'},
+	login:{name:'login', url:'../html/login.html'},
+	main:{name:'main', url:'../html/main.html'},
+	register:{name:'register', url:'../html/register.html'},
+	scanner:{name:'scanner', url:'../html/scanner.html'},
+	createEms:{name:'createEms', url:'../html/createEms.html'},
+	getInfo:{name:'getInfo', url:'../getInfo.html'},
+	selectProjname:{name:'selectProjname', url:'../html/selectProjname.html'}
+
+}
+function post(url,data,cb){
+		api.ajax({
+	           url:url,
+	           cache:false,
+	           method:'post',
+	           timeout:5,
+	           data:{values:data}
+         },cb);
+}
+
+function get(url,cb){
+	api.ajax({
+		url:url,
+		cache:false,
+		method:'get',
+		timeout:3
+	},cb);
+}
